3ff507e15b526a2d5f2d1297e2db2ef5cab15830
Omar-AlQasem <74102208+Omar-AlQasem@users.noreply.github.com>
2021-02-02 15:18:49 +0200

    Update app.jsx

--- pokemon-api/src/app.jsx
+++ pokemon-api/src/app.jsx
@@ -1,5 +1,21 @@
 import React,{useState} from 'react';
-
+// import axios from 'axios';
+// const Axios = (props) => {
+//     const [names, setNames] = useState([]);
+//     const getNames = () => {
+//     axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=807")
+//     .then(response => setNames(response.data.results))
+//     .catch(error => console.log(error))
+//     }
+// return (
+//         <div>
+//             <button onClick={ getNames }>Fetch Pokemon</button>
+//             {names.length > 0 && names.map((names, i)=>{
+//                 return (<div key={i}>{names.name}</div>)
+//             })}
+//         </div>
+//     );
+// };
 const Example = (props) => {
     const [poke, setPoke] = useState([]);
     const useEffect = () => {
